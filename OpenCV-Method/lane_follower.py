d77186b6bd718ae4d04fa0d5abccb6236a90fb80
Alif Abdullah <alifabdullah7040@gmail.com>
2023-08-14 18:24:36 -0400

    read in the forward image and created an inline todo 'ticket' to figure out how to crop the image

--- OpenCV-Method/lane_follower.py
+++ OpenCV-Method/lane_follower.py
@@ -12,3 +12,9 @@ else:
     # waits for a user to press a key, in this case for 1000 milliseconds
     cv.waitKey(1000)
     cv.destroyAllWindows()
+
+    # TODO:
+    # Figure out a way to crop the image so the lane lines are
+    # the central focus of your image.
+    # https://stackoverflow.com/questions/48301186/cropping-concave-polygon-from-image-using-opencv-python
+    # Adapt the above stackoverflow code into your current program
